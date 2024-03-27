002ec933ae5e13e9ba5563a1c1377289abcd7335
naseer6 <144029670+naseer6@users.noreply.github.com>
2024-03-27 10:24:24 +0100

    Update_assignment4

---
+++ SomerenUI/AddNewStudent.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace SomerenUI
+{
+    partial class AddNewStudent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new System.Windows.Forms.Label();
+            textBox1 = new System.Windows.Forms.TextBox();
+            textBox2 = new System.Windows.Forms.TextBox();
+            label2 = new System.Windows.Forms.Label();
+            textBox3 = new System.Windows.Forms.TextBox();
+            label3 = new System.Windows.Forms.Label();
+            textBox4 = new System.Windows.Forms.TextBox();
+            label4 = new System.Windows.Forms.Label();
+            button1 = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label1.Location = new System.Drawing.Point(109, 51);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(30, 21);
+            label1.TabIndex = 0;
+            label1.Text = "ID:";
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            textBox1.Location = new System.Drawing.Point(109, 81);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(183, 29);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            textBox2.Location = new System.Drawing.Point(109, 173);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(183, 29);
+            textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label2.Location = new System.Drawing.Point(109, 143);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(86, 21);
+            label2.TabIndex = 2;
+            label2.Text = "Full Name:";
+            // 
+            // textBox3
+            // 
+            textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            textBox3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            textBox3.Location = new System.Drawing.Point(109, 263);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new System.Drawing.Size(183, 29);
+            textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label3.Location = new System.Drawing.Point(109, 233);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(103, 21);
+            label3.TabIndex = 4;
+            label3.Text = "Tel. Number:";
+            // 
+            // textBox4
+            // 
+            textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            textBox4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            textBox4.Location = new System.Drawing.Point(109, 350);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new System.Drawing.Size(183, 29);
+            textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label4.Location = new System.Drawing.Point(109, 320);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(50, 21);
+            label4.TabIndex = 6;
+            label4.Text = "Class:";
+            // 
+            // button1
+            // 
+            button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            button1.Location = new System.Drawing.Point(134, 419);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(129, 50);
+            button1.TabIndex = 8;
+            button1.Text = "ADD";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // AddNewStudent
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(403, 505);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "AddNewStudent";
+            Text = "AddNewStudent";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
